0ceb44e5751667d2ea15e9fa321481444589f681
xakepa <plamen_ivanov@mail.com>
2021-12-03 18:40:19 +0200

    testimonials ready

---
+++ scripts/testimonials-slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".testimonials").slick({
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    mobileFirst: true,
+    pauseOnHover: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
